df7eb2bd06c97e3d9ebbfdf80e024bccc11c7124
rvronski <rvronski@gmail.com>
2023-05-03 16:39:45 +0500

    Initial Commit

---
+++ RPTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPTest
+//
+//  Created by ROMAN VRONSKY on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
