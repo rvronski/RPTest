20a649568d09f59785f34bfc9af7c711e606405e
rvronski <rvronski@gmail.com>
2023-05-03 17:40:57 +0500

    Create CoreDataManager

---
+++ RPTest/CoreData/CoreDataManager.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataManager.swift
+//  RPTest
+//
+//  Created by ROMAN VRONSKY on 03.05.2023.
+//
+
+import Foundation
+import CoreData
+
+protocol CoreDataManagerProtocol: AnyObject {
+    
+}
+
+class CoreDataManager: CoreDataManagerProtocol {
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "RPTest")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+               
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}
---
+++ RPTest/Screens/Home/HomeView.swift
@@ -0,0 +1,14 @@
+//
+//  HomeView.swift
+//  RPTest
+//
+//  Created by ROMAN VRONSKY on 03.05.2023.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    
+
+}
